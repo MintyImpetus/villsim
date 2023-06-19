1ce5a576580847d2dca965d4264421b348c9efa5
MintyImpetus <mi@mimpetus.com>
2023-06-19 18:00:01 +0930

    Added initial code for bubbletea library

--- clientvillsim.go
+++ clientvillsim.go
@@ -7,6 +7,39 @@ import (
     tea "github.com/charmbracelet/bubbletea"
 )
 
+type model struct {
+}
+
+func initialModel() model {
+	return model{
+	}
+}
+
+func (m model) Init() tea.Cmd {
+    // Just return `nil`, which means "no I/O right now, please."
+    return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+    case tea.KeyMsg:
+        switch msg.String() {
+        case "ctrl+c", "q":
+            return m, tea.Quit
+        }
+    }
+    return m, nil
+}
+
+func (m model) View() string {
+    s := "Placeholder\n\n"
+    return s
+}
+
 func main() {
-	fmt.Println("Hello World...")
+    p := tea.NewProgram(initialModel())
+    if _, err := p.Run(); err != nil {
+        fmt.Printf("Alas, there's been an error: %v", err)
+        os.Exit(1)
+    }
 }
